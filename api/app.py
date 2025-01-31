fde74ce923083520b3380a67ecd77e6a2f138800
littlefieldnick <ngl18@pitt.edu>
2025-01-31 16:43:40 -0500

    Basic react front end

--- api/app.py
+++ api/app.py
@@ -4,8 +4,26 @@ from flask_cors import CORS
 import os
 import sys
 
-app = Flask(__name__)
+from chat import ask_question
+from flask import Flask, Response, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__, static_folder="../frontend/build", static_url_path="/")
 CORS(app)
+
+@app.route("/")
+def api_index():
+    return app.send_static_file("index.html")
+
+@app.route("/api/chat", methods=["POST"])
+def api_chat():
+    request_json = request.get_json()
+    question = request_json.get("question")
+    if question is None:
+        return jsonify({"msg": "Missing question from request JSON"}), 400
+
+    session_id = request.args.get("session_id", str(uuid4()))
+    return Response(ask_question(question, session_id), mimetype="text/event-stream")
 
 @app.cli.command()
 def create_index():
